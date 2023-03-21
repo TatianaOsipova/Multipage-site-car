be1beb83664bc444757f4d06b5ec38e6490e68fa
Tatiana Osipova <otv120780@gmail.com>
2023-03-21 10:10:05 -0400

    feat: add file main.js to pages contacts and new-cars

--- js/main.js
+++ js/main.js
@@ -15,3 +15,13 @@ const tabContent = document.querySelectorAll('.tabs__content-item');
 tabItem.forEach(function (element) {
     element.addEventListener('click', open);
 })
+
+function open(evt){
+    const tabTarget = evt.currentTarget;
+
+    tabItem.forEach(function(item) {
+        item.classList.remove('tabs__btn-item--active');
+    })
+
+    tabTarget.classList.add('tabs__btn-item--active');
+}
