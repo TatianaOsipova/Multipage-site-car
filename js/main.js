0937373a1a56c84f31acfffb2398d99bed4a8d9b
Tatiana Osipova <otv120780@gmail.com>
2023-03-23 10:51:26 -0400

    feat: add media 768px

--- js/main.js
+++ js/main.js
@@ -5,23 +5,30 @@ tabItem.forEach(function (element) {
     element.addEventListener('click', open);
 })
 
-function open(evt){
+function open(evt) {
     const tabTarget = evt.currentTarget;
     const button = tabTarget.dataset.button;
 
-    tabItem.forEach(function(item) {
+    tabItem.forEach(function (item) {
         item.classList.remove('tabs__btn-item--active');
     });
 
     tabTarget.classList.add('tabs__btn-item--active');
 
-    tabContent.forEach(function(item) {
+    tabContent.forEach(function (item) {
         item.classList.remove('tabs__content-item--active');
     });
 
     document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
-
 }
+
+const menuBtn = document.querySelector('.menu__btn');
+const menu = document.querySelector('.menu__list');
+
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('menu__list--active');
+});
+
 
 const swiper = new Swiper(".swiper", {
     effect: "fade",
